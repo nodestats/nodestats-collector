013b66d8232fffe99523236e63dab8b991e20c54
D. Rimron-Soutter <darran@xalior.com>
2017-05-04 01:30:16 +0100

    API posting client, V0.0.3 :D
    -Dx

--- server.js
+++ server.js
@@ -10,16 +10,17 @@ const await = require('asyncawait/await');
 
 const VERSION_MAJOR = 0;
 const VERSION_MINOR = 0;
-const VERSION_PATCH = 2;
+const VERSION_PATCH = 3;
 
 // Override these by a config JSON object, see config.json.example for details
 
 let config = {
-    "port"  :   57475,
-    "listen":   true,
-    "ip"    :   "127.0.0.1",
-    "drives":   "/dev/",
-    "repeat":   60
+    "portno"      :   57475,
+    "listen"      :   false,
+    "ipaddr"      :   "127.0.0.1",
+    "drives"      :   "/dev/",
+    "server"      :   "localhost",
+    "srvprt"      :   57473
 };
 
 try {
@@ -30,25 +31,40 @@ try {
     console.info("  --Time to die. x");
     process.exit();
 }
-
-const updater = setTimeout(async(function(){
-    stats = await(mkStats());
-    if(config.apikey.length) {
-        // POST "/ping"
-    };
-// EVERY 60 SECONDS
-}, config.repeat*1000));
+if((config.listen) || (config.apikey) && (config.server) && (config.srvprt)) {
+    setTimeout(async(function(){
+        stats = await(mkStats());
+        if((config.apikey) && (config.server) && (config.portno)) {
+            // If we have a key & server, they probably want us to post to something...
+            let options = {
+                hostname: config.server,
+                port: config.srvprt,
+                path: '/',
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                }
+            };
+            console.log(options);
+            // POST "/ping"
+            let req = http.request(options, function(res) {
+            });
+            req.on('error', function(e) {
+                console.log('problem with request: ' + e.message);
+            });
+            req.write(JSON.stringify(stats));
+            req.end();
+        }
+    // EVERY 60 SECONDS
+    }, config.repeat*1000));
+}
 
 const driveCheck = new RegExp(config.drives);
 const mkDrives =() => {
-    return new Promise(function (resolve, reject) {
-        var drives = [];
-
+    return new Promise(function (resolve) {
+        let drives = [];
         diskinfo.getDrives(function (err, aDrives) {
             aDrives.forEach((drive) => {
-                console.log(driveCheck);
-                console.log(drive.filesystem);
-                console.log(drive.filesystem.match(driveCheck));
                 if(drive.filesystem.match(driveCheck)) drives.push(drive);
             });
             resolve( drives );
@@ -58,45 +74,40 @@ const mkDrives =() => {
 
 const mkStats = async(() => {
     let DRIVES = await(mkDrives());
-    console.log(DRIVES);
     return {
-        'VERSION_MAJOR': VERSION_MAJOR,
-        'VERSION_MINOR': VERSION_MINOR,
-        'VERSION_PATCH': VERSION_PATCH,
-        'HOSTNAME': config.hostname ? config.hostname : os.hostname(),
-        'OS': os.platform(),
-        'ARCH': os.arch(),
-        'RELEASE': os.release(),
-        'UPTIME': os.uptime(),
-        'TOTALMEMORY': os.totalmem(),
-        'FREEMEM': os.freemem(),
-        'CPU': os.loadavg()[0],
-        'DRIVES': DRIVES,
-        'INTERACES': os.networkInterfaces(),
-        'IP': config.ip,
-        'PORT': config.port,
-        'TIMESTAMP': new Date().toISOString()
+        'VERSION_MAJOR' : VERSION_MAJOR,
+        'VERSION_MINOR' : VERSION_MINOR,
+        'VERSION_PATCH' : VERSION_PATCH,
+        'HOSTNAME'      : config.myname ? config.myname : os.hostname(),
+        'OS'            : os.platform(),
+        'ARCH'          : os.arch(),
+        'RELEASE'       : os.release(),
+        'UPTIME'        : os.uptime(),
+        'TOTALMEMORY'   : os.totalmem(),
+        'FREEMEM'       : os.freemem(),
+        'CPU'           : os.loadavg()[0],
+        'DRIVES'        : DRIVES,
+        'INTERACES'     : os.networkInterfaces(),
+        'IP'            : config.ipaddr,
+        'PORT'          : config.portno,
+        'TIMESTAMP'     : new Date().toISOString()
     };
 });
-
-// parse config here...
 
 let stats = mkStats();
 
 const requestHandler = (req, res) => {
-    console.log("REQ: "+req.url);
-    console.log(stats);
     res.setHeader('Content-Type', 'application/json');
     res.end(JSON.stringify(stats));
 };
 
 const server = http.createServer(requestHandler);
 
+// Should we start the HTTP server... if you have not set a SERVER and APIKEY then this is the ONLY use we have...
 if(config.listen) {
-    server.listen(config.port ? config.port : DEFAULT_CONFIG.port,
-        config.ip ? config.ip : DEFAULT_CONFIG.ip);
+    server.listen(config.portno, config.ipaddr);
+    console.log("nodestats-collector is listening on http://"+config.ipaddr+":"+config.portno+"/");
 }
 
-console.log("nodestats-collector is listening on http://"+config.ip+":"+config.port+"/");
 
 
